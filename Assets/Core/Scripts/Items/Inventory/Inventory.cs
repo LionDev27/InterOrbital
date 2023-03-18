380b5ad8241fbf4b78c4a07b91de3d30f1194ecc
Santiago Muñoz Castro <santiyagito22@gmail.com>
2023-03-18 17:39:48 +0100

    Inventario fase 1 finalizado
    Añadido inventario rápido y arreglado el bug visual de cuando se añadian items

--- Assets/Core/Scripts/Items/Inventory/Inventory.cs
+++ Assets/Core/Scripts/Items/Inventory/Inventory.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using InterOrbital.Item;
 using InterOrbital.UI;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.Utilities;
 using UnityEngine.UI;
 
 namespace InterOrbital.Player
@@ -12,18 +14,27 @@ namespace InterOrbital.Player
     public class Inventory : PlayerComponents
     {
         private int _totalNumberOfSlots;
-        public ItemScriptableObject[] _items;
+        private ItemScriptableObject[] _items;
         private Image[] _itemsSlot;
+        private Image[] _itemsFastInventory;
         private int _level;
-        private int sizeInventory;
+        private int _sizeInventory;
+        private int _numSlotsFastInventory;
+        
+        //Como el vector items esta ordenado, solo basta con usar un numero del 0 al _numSlotsFastInventory para saber que items usar
+        //Me ahorro crear asi otros arrays para la gestion del inventario rapido. Solo me hace falta el de las imagenes a mostrar
+        private int _actualItemEquiped; 
         
         public GameObject gridMain;
         public GameObject gridLeftPocket;
         public GameObject gridRightPocket;
-        
+        public GameObject gridFastInventory;
+            
         public GameObject bagUI;
         public ItemScriptableObject itemVoid;
         public ItemScriptableObject itemTest;
+
+        
 
         protected override void Awake()
         {
@@ -34,13 +45,14 @@ namespace InterOrbital.Player
         private void Start()
         {
             _level = 1;
+            _numSlotsFastInventory = gridFastInventory.transform.childCount;
             InitSlots();
             _items = new ItemScriptableObject[_totalNumberOfSlots];
             for (int i = 0; i < _items.Length; i++)
             {
                 _items[i] = itemVoid;
             }
-            sizeInventory = gridMain.transform.childCount;
+            _sizeInventory = gridMain.transform.childCount;
         }
 
         private void InitSlots()
@@ -48,23 +60,28 @@ namespace InterOrbital.Player
             var sizeMain= gridMain.transform.childCount;
             var sizeLeft= gridLeftPocket.transform.childCount;
             var sizeRight = gridRightPocket.transform.childCount;
-            
+           
             _totalNumberOfSlots = sizeMain + sizeLeft  +  sizeRight;
             _itemsSlot = new Image[_totalNumberOfSlots];
-            RelateSlots(gridMain, 0,sizeMain);
-            RelateSlots(gridLeftPocket, sizeMain,sizeLeft );
-            RelateSlots(gridRightPocket, sizeMain + sizeLeft,sizeRight);
+            _itemsFastInventory = new Image[_numSlotsFastInventory];
+            
+            
+            RelateSlots(gridMain, 0,sizeMain, _itemsSlot);
+            RelateSlots(gridLeftPocket, sizeMain,sizeLeft,_itemsSlot );
+            RelateSlots(gridRightPocket, sizeMain + sizeLeft,sizeRight,_itemsSlot);
+            RelateSlots(gridFastInventory, 0, 5, _itemsFastInventory);
+            
           
         }
 
-        private void RelateSlots(GameObject grid,int startSize, int size)
+        private void RelateSlots(GameObject grid,int startSize, int size, Image[] imagesSlot)
         {
             for (var i = 0; i < size; i++)
             { 
-                _itemsSlot[i + startSize] = grid.transform.GetChild(i).GetChild(0).GetComponent<Image>();
+                imagesSlot[i + startSize] = grid.transform.GetChild(i).GetChild(0).GetComponent<Image>();
             }
         }
-
+        
         
         private void Update()
         {
@@ -81,18 +98,9 @@ namespace InterOrbital.Player
         private void UpdateInventory()
         {
             UIManager.Instance.ActivateOrDesactivateUI(bagUI);
-            UpdateImages();
+            
         }
-
-        private void UpdateImages()
-        {
-            for (int i = 0; i < _items.Length; i++)
-            {
-                Debug.Log(_items[i].name);
-                _itemsSlot[i].sprite = _items[i].itemSprite;
-            }
-        }
-
+        
         private void UpdateLevel()
         {
             if (_level < 3)
@@ -100,11 +108,19 @@ namespace InterOrbital.Player
                 _level++;
                 UIManager.Instance.ActivateOrDesactivateUI(_level==2 ? gridLeftPocket : gridRightPocket);
                 if (_level == 2)
-                    sizeInventory += gridLeftPocket.transform.childCount;
+                    _sizeInventory += gridLeftPocket.transform.childCount;
                 else
                 {
-                    sizeInventory += gridRightPocket.transform.childCount;
+                    _sizeInventory += gridRightPocket.transform.childCount;
                 }
+            }
+        }
+        
+        private void UpdateFastInventory()
+        {
+            for (int i = 0; i < _numSlotsFastInventory; i++)
+            {
+                _itemsFastInventory[i].sprite = _items[i].itemSprite;
             }
         }
 
@@ -112,28 +128,33 @@ namespace InterOrbital.Player
         public void AddItem(ItemScriptableObject item)
         {
             
-            for(int i=0; i< sizeInventory; i++)
+            for(int i=0; i< _sizeInventory; i++)
             {
                 if (_items[i] == itemVoid)
                 {
                     _items[i] = item;
+                    _itemsSlot[i].sprite = _items[i].itemSprite;
+
+                    if (i < _numSlotsFastInventory)
+                    {
+                        _itemsFastInventory[i].sprite = _items[i].itemSprite;
+                    }
                     break;
                 }
-            }
-
-            if(bagUI.activeSelf)
-            {
-                UpdateImages();
             }
         }
 
         public void SwitchItems(int indexA, int indexB)
         {
-           ItemScriptableObject auxItem= _items[indexB];
-           _items[indexB] = _items[indexA];
-           _items[indexA] = auxItem;
-           //Debug.Log("Intercambiamos "+indexA+", "+indexB);
+            (_itemsSlot[indexB], _itemsSlot[indexA]) = (_itemsSlot[indexA], _itemsSlot[indexB]);
+            (_items[indexB], _items[indexA]) = (_items[indexA], _items[indexB]);
+            if (indexA < _numSlotsFastInventory || indexB < _numSlotsFastInventory)
+            {
+                UpdateFastInventory();
+            }
         }
+
+        
         
     }
 }
