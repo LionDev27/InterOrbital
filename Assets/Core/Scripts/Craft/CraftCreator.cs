fd03f6517e80dc4e6a2ade7eb474699cc971db76
Santiago Muñoz Castro <santiyagito22@gmail.com>
2023-05-02 16:30:38 +0200

    Formula crafteo(!/3)

--- Assets/Core/Scripts/Craft/CraftCreator.cs
+++ Assets/Core/Scripts/Craft/CraftCreator.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using InterOrbital.Item;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CraftCreator : MonoBehaviour
 {
+    public List<Image> requireImages;
+    public List<TextMeshProUGUI> requireTexts;
+    
     public void SelectCraft(ItemCraftScriptableObject itemCraft)
     {
-        Debug.Log("Tiene "+itemCraft.itemsRequired.Count);
+        for(int i=0; i< itemCraft.itemsRequired.Count; i++)
+        {
+            requireImages[i].sprite = itemCraft.itemsRequired[i].item.itemSprite;
+        }
     }
+
+    
 }
