4db7c08c3e8d69663f789d73ef00d026d0024710
León Zhou <leonzhou2b@gmail.com>
2023-05-11 17:26:31 +0200

    fix: dash cooldown added

--- Assets/Core/Scripts/Combat/DamageDealers/DamageDealer.cs
+++ Assets/Core/Scripts/Combat/DamageDealers/DamageDealer.cs
@@ -17,15 +17,9 @@ namespace InterOrbital.Combat
         protected virtual void CheckCollision(Collider2D other)
         {
             //Si este DamageDealer tiene asignado un attackerTag, lo comprueba. Si no, comprueba su propio tag.
-            if ((attackerTag != "" && other.CompareTag(attackerTag)) || other.CompareTag(gameObject.tag))
-            {
-                Debug.Log("No puede atacarse a si mismo ni a otro objeto con el mismo tag.");
-            }
-            else if (other.CompareTag("Player") && other is BoxCollider2D)
-            {
-                Debug.Log("No puede atacarse al BoxCollider del jugador.");
-            }
-            else if (other.TryGetComponent(out Damageable damageable))
+            if ((attackerTag != "" && other.CompareTag(attackerTag)) || other.CompareTag(gameObject.tag)) return;
+            if (other.CompareTag("Player") && other is BoxCollider2D) return;
+            if (other.TryGetComponent(out Damageable damageable))
             {
                 AttackDamageableTarget(other, damageable);
             }
