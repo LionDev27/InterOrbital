a9bf054a6aca0f844bc0b45d0ab0e577ed579a70
Santiago Muñoz Castro <santiyagito22@gmail.com>
2023-05-14 16:32:25 +0200

    Mejorado metodo para obtener root en inventario

--- Assets/Core/Scripts/Items/DraggableItem.cs
+++ Assets/Core/Scripts/Items/DraggableItem.cs
@@ -5,27 +5,45 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using InterOrbital.Player;
 using InterOrbital.Utils;
+using System;
 
-    public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
 
-    [SerializeField] private Transform rootCanvas;
     public Image image;
     [HideInInspector] public Transform parentAfterDrag;
     public int inventoryIndex;
     
+    private Transform GetRootParent()
+    {
+        Transform root=transform.parent;
+        for(int i=0; i<10; i++)
+        {
+            if(root.GetComponent<Canvas>() != null)
+            {
+                return root;
+            }
+            else
+            {
+                root = root.parent;
+            }
+        }
+
+        return root;
+    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         if (!PlayerComponents.Instance.Inventory.isHide)
         {
             parentAfterDrag = transform.parent;
-            transform.SetParent(rootCanvas);
+            transform.SetParent(GetRootParent());
             transform.SetAsLastSibling();
             image.raycastTarget = false;
         }
     }
-    
+
+
     public void OnDrag(PointerEventData eventData)
     {
         if (!PlayerComponents.Instance.Inventory.isHide)
