7922e21aea9d690851e1c4ca0af0684eea9824f1
León Zhou <leonzhou2b@gmail.com>
2023-06-19 17:33:05 +0200

    fix: Elytrin alert state bug

--- Assets/Core/Scripts/Combat/IA/EnemyAgentBase.cs
+++ Assets/Core/Scripts/Combat/IA/EnemyAgentBase.cs
@@ -45,8 +45,12 @@ namespace InterOrbital.Combat.IA
                 _animator.SetBool("Hit", false);
                 EnableNavigation(true);
             }
+
             if (_currentState)
+            {
                 _currentState.Execute();
+                Debug.Log(_currentState);
+            }
         }
 
         public virtual void ChangeState(EnemyStateBase newState)
@@ -57,7 +61,7 @@ namespace InterOrbital.Combat.IA
 
         public bool ArrivedDestination()
         {
-            return _navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance &&
+            return _navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance ||
                    (!_navMeshAgent.hasPath || _navMeshAgent.velocity.sqrMagnitude == 0f);
         }
 
