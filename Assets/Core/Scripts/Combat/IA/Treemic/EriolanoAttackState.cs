18cee7a55bb248d8f00743405d7236a7b88e9aea
León Zhou <leonzhou2b@gmail.com>
2023-10-03 11:40:33 +0200

    Eriolano Attack

--- Assets/Core/Scripts/Combat/IA/Treemic/EriolanoAttackState.cs
+++ Assets/Core/Scripts/Combat/IA/Treemic/EriolanoAttackState.cs
@@ -5,11 +5,26 @@ namespace InterOrbital.Combat.IA
 {
     public class EriolanoAttackState : TreemicAttackState
     {
+        [SerializeField] private float _attackAngle;
+        
         public override void Attack()
         {
-            var tempBullet = Instantiate(_bulletPrefab, _bulletTransformSpawn.position, Quaternion.identity);
-            var bulletController = tempBullet.GetComponent<BaseBulletController>();
-            bulletController.SetupBullet(gameObject.tag, TargetDir(), transform.position);
+            var currentAngle = _attackAngle;
+
+            for (int i = 0; i < 3; i++)
+            {
+                var tempBullet = Instantiate(_bulletPrefab, _bulletTransformSpawn.position, Quaternion.identity);
+                var bulletController = tempBullet.GetComponent<BaseBulletController>();
+                bulletController.SetupBullet(gameObject.tag, GetAngleDir(currentAngle).normalized, transform.position);
+                currentAngle -= _attackAngle;
+            }
+        }
+
+        private Vector2 GetAngleDir(float angle)
+        {
+            if (angle == 0f)
+                return TargetDir();
+            return (Quaternion.AngleAxis(angle, Vector3.forward) * TargetDir());
         }
     }
 }
