68136c3fda7a5fbad8022691e39e55f6876d0fad
León Zhou <leonzhou2b@gmail.com>
2023-11-07 18:41:57 +0100

    Order in layer fix

--- Assets/Core/Scripts/Others/OrderInLayerController.cs
+++ Assets/Core/Scripts/Others/OrderInLayerController.cs
@@ -1,32 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using InterOrbital.WorldSystem;
 using UnityEngine;
 
 public class OrderInLayerController : MonoBehaviour
 {
     [SerializeField] private bool isStatic;
-    private bool _canChange;
+    [SerializeField] private int _offset;
+    
     private SpriteRenderer _spriteRenderer;
-
-    // Start is called before the first frame update
+    private bool _canChange;
+    private int _maxY;
+    private int _lastOrder;
+    private float _timer;
+    private float _timerMax = .1f;
 
     private void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
     }
+    
     void Start()
     {
         _canChange = true;
-        _spriteRenderer.sortingOrder = (int)transform.position.y;
+        //Multiplicamos por 2 para no llegar nunca al order 0, por si acaso.
+        _maxY = GridLogic.Instance.height * 2;
+        ChangeSortingOrder();
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        if (!isStatic && _canChange)
+        if (_timer <= 0f)
         {
-            _spriteRenderer.sortingOrder = (int)transform.position.y;
+            if (CurrentOrder() != _lastOrder && _canChange)
+                ChangeSortingOrder();
         }
+        else
+            _timer -= Time.deltaTime;
+    }
+
+    private void ChangeSortingOrder()
+    {
+        _spriteRenderer.sortingOrder = CurrentOrder();
+        _lastOrder = _spriteRenderer.sortingOrder;
+        _timer = _timerMax;
+    }
+
+    private int CurrentOrder()
+    {
+        return _maxY - (int)transform.position.y - _offset;
     }
 
     public void SetCanChange(bool v)
